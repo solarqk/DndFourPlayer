e7f9630e5144fc74cdd4ba97a3cc90da743e6014
solarqk <koehlecarson@gmail.com>
2026-02-18 17:29:04 -0700

    more more changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(() => {
-  const base = process.env.VITE_BASE ?? "/"; // default for local/K8s
-  return {
-    plugins: [react()],
-    base,
-  };
+export default defineConfig({
+  base: "/DndFourPlayer/",
+  plugins: [react()],
 });
